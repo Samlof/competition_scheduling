c758451ae2d14aa1e910ce080f001ab46e58732b
Samuli Löf <samuli.lof@gmail.com>
2018-03-28 00:00:33 +0300

    Refactor at TeamCountErrors

--- src/com/samuli/TeamCountErrors.java
+++ src/com/samuli/TeamCountErrors.java
@@ -7,8 +7,8 @@ public class TeamCountErrors {
     private int[] teamGameCounts;
 
     // Tracks the error count if team has over 2 or more games in this round
-    private int totalGameErrors_GameCount;
-    private int[] errorsByTeam_GameCount;
+    private int totalGameErrors;
+    private int[] errorsByTeam;
 
     // This should be a reference to Round::games list. Don't edit it from this class!
     private final ArrayList<Game> games;
@@ -16,14 +16,14 @@ public class TeamCountErrors {
     public TeamCountErrors(ArrayList<Game> pGames) {
         games = pGames;
         teamGameCounts = Team.getIntArray();
-        errorsByTeam_GameCount = Team.getIntArray();
+        errorsByTeam = Team.getIntArray();
 
         // Set up the team count errors
         // Empty round has no errors
-        totalGameErrors_GameCount = 0;
+        totalGameErrors = 0;
 
-        for (int i = 0; i < errorsByTeam_GameCount.length; i++) {
-            errorsByTeam_GameCount[i] = 0;
+        for (int i = 0; i < errorsByTeam.length; i++) {
+            errorsByTeam[i] = 0;
         }
     }
 
@@ -42,8 +42,8 @@ public class TeamCountErrors {
         int id = team.id;
         if (teamGameCounts[id] > 0) {
             // Already has one game with this team. Add an error
-            totalGameErrors_GameCount++;
-            errorsByTeam_GameCount[id]++;
+            totalGameErrors++;
+            errorsByTeam[id]++;
         }
         teamGameCounts[id]++;
     }
@@ -57,8 +57,8 @@ public class TeamCountErrors {
         int id = team.id;
         if (teamGameCounts[id] > 1) {
             // Already has one game with this team. Remove an error
-            totalGameErrors_GameCount--;
-            errorsByTeam_GameCount[id]--;
+            totalGameErrors--;
+            errorsByTeam[id]--;
         } else if (teamGameCounts[id] == 0) {
             // We should never get here, so print a message to show if it happened
             System.out.println("TeamCountErrors::removeTeam tried to remove a team without any games!");
@@ -67,11 +67,11 @@ public class TeamCountErrors {
     }
 
     public int getTotalTeamCountErrors() {
-        return totalGameErrors_GameCount;
+        return totalGameErrors;
     }
 
     public int getTeamCountsErrorByGame(Game game) {
-        return errorsByTeam_GameCount[game.home.id] + errorsByTeam_GameCount[game.guest.id];
+        return errorsByTeam[game.home.id] + errorsByTeam[game.guest.id];
     }
 
     // For debugging
@@ -79,30 +79,30 @@ public class TeamCountErrors {
         for (int i = 0; i < teamGameCounts.length; i++) {
             int c = teamGameCounts[i];
             if (c == 0) {
-                if (errorsByTeam_GameCount[i] != 0) {
-                    System.out.println("if(errorsByTeam_GameCount[i] != 1) {");
+                if (errorsByTeam[i] != 0) {
+                    System.out.println("if(errorsByTeam[i] != 1) {");
                     return false;
                 }
             }
             if (c > 1) {
-                if (errorsByTeam_GameCount[i] != c - 1) {
-                    System.out.println("if(errorsByTeam_GameCount[i] != c - 1) {");
+                if (errorsByTeam[i] != c - 1) {
+                    System.out.println("if(errorsByTeam[i] != c - 1) {");
                     return false;
                 }
             }
             if (c == 1) {
-                if (errorsByTeam_GameCount[i] != 0) {
-                    System.out.println("if(errorsByTeam_GameCount[i] != 0 {");
+                if (errorsByTeam[i] != 0) {
+                    System.out.println("if(errorsByTeam[i] != 0 {");
                     return false;
                 }
             }
         }
         int total = 0;
-        for (int e : errorsByTeam_GameCount) {
+        for (int e : errorsByTeam) {
             total += e;
         }
-        if (totalGameErrors_GameCount != total) {
-            System.out.println(totalGameErrors_GameCount + "(totalGameErrors_GameCount != total) {" + total);
+        if (totalGameErrors != total) {
+            System.out.println(totalGameErrors + "(totalGameErrors != total) {" + total);
             return false;
         }
         return true;
